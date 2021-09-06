2d9397b53086bddc7449f977f2c7eabbf94b645e
Abel Diress <abeldiress05@gmail.com>
2021-09-06 11:48:09 -0400

    added fonts library

--- components/atoms/btn-primary/btn-primary.js
+++ components/atoms/btn-primary/btn-primary.js
@@ -1,10 +1,27 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
+import colours from "../../../themes/colours";
 
 const BtnPrimary = ({ text, onPress }) => {
   return (
-    <Button onPress={onPress}>
-      <Text>{text}</Text>
-    </Button>
+    <TouchableOpacity onPress={onPress} style={styles.primary_button}>
+      <Text style={styles.text}>{text}</Text>
+    </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  primary_button: {
+    width: "100%",
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: colours.primary,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: colours.white,
+  },
+});
+
+export default BtnPrimary;
---
+++ themes/fonts.js
@@ -0,0 +1,65 @@
+import {
+  useFonts,
+  DMSans_400Regular,
+  DMSans_500Medium,
+  DMSans_700Bold,
+} from "@expo-google-fonts/dm-sans";
+
+let [fontsLoaded] = useFonts({
+  DMSans_400Regular,
+  DMSans_500Medium,
+  DMSans_700Bold,
+});
+
+const fonts = {
+  BigBold: {
+    fontSize: 30,
+    fontFamily: "DMSans_700Bold",
+  },
+  BigMedium: {
+    fontSize: 30,
+    fontFamily: "DMSans_500Medium",
+  },
+  BigRegular: {
+    fontSize: 30,
+    fontFamily: "DMSans_400Regular",
+  },
+  MedBold: {
+    fontSize: 16,
+    fontFamily: "DMSans_700Bold",
+  },
+  MedMedium: {
+    fontSize: 16,
+    fontFamily: "DMSans_500Medium",
+  },
+  MedRegular: {
+    fontSize: 16,
+    fontFamily: "DMSans_400Regular",
+  },
+  SmallBold: {
+    fontSize: 14,
+    fontFamily: "DMSans_700Bold",
+  },
+  SmallMedium: {
+    fontSize: 14,
+    fontFamily: "DMSans_500Medium",
+  },
+  SmallRegular: {
+    fontSize: 14,
+    fontFamily: "DMSans_400Regular",
+  },
+  TinyBold: {
+    fontSize: 10,
+    fontFamily: "DMSans_700Bold",
+  },
+  TinyMedium: {
+    fontSize: 10,
+    fontFamily: "DMSans_500Medium",
+  },
+  TinyRegular: {
+    fontSize: 10,
+    fontFamily: "DMSans_400Regular",
+  },
+};
+
+export default fonts;
