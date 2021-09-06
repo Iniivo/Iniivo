cbe544f7c76f7e0d82283cedeeffe3f7eee9ce72
Abel Diress <abeldiress05@gmail.com>
2021-09-06 12:03:50 -0400

    onpress change

--- components/atoms/btn-primary/btn-primary.js
+++ components/atoms/btn-primary/btn-primary.js
@@ -1,18 +1,30 @@
-import React from "react";
-import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableHighlight,
+} from "react-native";
 import colours from "../../../themes/colours";
 import fonts from "../../../themes/fonts";
 
 const BtnPrimary = ({ text, onPress }) => {
+  const [isPress, setIsPress] = useState(false);
   return (
-    <TouchableOpacity onPress={onPress} style={styles.primary_button}>
-      <Text style={styles.text}>{text}</Text>
-    </TouchableOpacity>
+    <TouchableHighlight
+      onHideUnderlay={() => setIsPress(false)}
+      onShowUnderlay={() => setIsPress(true)}
+      onPress={onPress}
+      style={isPress ? styles.btnPress : styles.btnNorm}
+    >
+      <Text style={isPress ? styles.textPress : styles.textNorm}>{text}</Text>
+    </TouchableHighlight>
   );
 };
 
 const styles = StyleSheet.create({
-  primary_button: {
+  btnNorm: {
     width: "100%",
     height: 50,
     borderRadius: 25,
@@ -20,8 +32,20 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  text: {
+  btnPress: {
+    width: "100%",
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: colours.light_grey,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textNorm: {
     color: colours.white,
+    ...fonts.MedBold,
+  },
+  textPress: {
+    color: colours.primary,
     ...fonts.MedBold,
   },
 });
